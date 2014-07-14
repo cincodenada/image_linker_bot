44ffae1dce0221a3534e300103b0b4bcf5db3ca9
Joel Bradshaw <cincodenada@gmail.com>
2014-07-13 18:49:39 -0700

    Add util.py

---
+++ util.py
@@ -0,0 +1,26 @@
+### File borrowed from autowikibot @ https://github.com/acini/autowikibot-py
+### Which was in turn borrowed from Zack Maril @ https://github.com/zmaril
+import re, time
+
+def formatted(*args):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    return "["+now+"] "+" ".join(map(str,args))
+
+
+def log(*args):
+    print apply(formatted,args)
+
+def fail(*args):
+    print '\033[91m'+apply(formatted,args)+'\033[0m'
+
+def warn(*args):
+    print '\033[93m'+apply(formatted,args)+'\033[0m'
+
+def success(*args):
+    print '\033[92m'+apply(formatted,args)+'\033[0m'
+    
+def special(*args):
+    print '\033[95m'+apply(formatted,args)+'\033[0m'
+    
+def bluelog(*args):
+    print '\033[94m'+apply(formatted,args)+'\033[0m'
