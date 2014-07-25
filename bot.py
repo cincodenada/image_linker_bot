f03b9915c97a9c00132077fe83e4f584323332c2
Joel Bradshaw <cincodenada@gmail.com>
2014-07-25 00:16:25 -0700

    Refactor reusable stuff into its own class

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 # vim: sw=2 ts=2 sts=2 et :
-import time
 import praw
+import time
 import re 
 import sys
 import argparse
 import yaml
-import json
 from pprint import pprint
 import random
 import collections
-import pickle
 import signal
-from util import success, warn, log, fail
 import shutil
+
+from joelbot import JoelBot
 
 def load_imagelist(config):
   matchlist = {}
@@ -53,168 +52,56 @@ def print_imagelist(config, format = 'markdown'):
 
 def form_reply(link_list):
   lines = ["[%s](%s)  " % keyval for keyval in link_list.iteritems()]
-  reply = "\n".join(lines) + "\n\n" + config['bot']['footer']
+  reply = "\n".join(lines) + "\n\n" + bot.config['bot']['footer']
   return reply
-
-def cleanup():
-  global last_cleaned
-
-  if(last_cleaned and last_cleaned > (time.time() - config['bot']['cleanup_time'])):
-    return
-
-  subreddit_scores = {}
-  last_cleaned = time.time()
-
-  # Comment deletion taken straight from autowikibot
-  # No need to reinvent the wheel
-  log("COMMENT SCORE CHECK CYCLE STARTED")
-  user = r.get_redditor(config['account']['username'])
-  total = 0
-  upvoted = 0
-  unvoted = 0
-  downvoted = 0
-  deleted = 0
-  del_list = []
-          
-  for c in user.get_comments(limit=None):
-    
-    if len(str(c.score)) == 4:
-      spaces = ""
-    if len(str(c.score)) == 3:
-      spaces = " "
-    if len(str(c.score)) == 2:
-      spaces = "  "
-    if len(str(c.score)) == 1:
-      spaces = "   "
-    
-    #Keep track of our votes
-    sub = c.subreddit.display_name
-    if(sub not in subreddit_scores):
-      subreddit_scores[sub] = 0
-    subreddit_scores[sub] += c.score
-
-    total = total + 1
-
-    if c.score < 1: # or sub.lower() in bans:
-      del_list.append((sub.lower(), c.score, c.permalink))
-      c.delete()
-      print "\033[1;41m%s%s\033[1;m"%(spaces,c.score),
-      deleted = deleted + 1
-      downvoted = downvoted + 1
-    elif c.score > 10:
-      print "\033[1;32m%s%s\033[1;m"%(spaces,c.score),
-      upvoted = upvoted + 1
-    elif c.score > 1:
-      print "\033[1;34m%s%s\033[1;m"%(spaces,c.score),
-      upvoted = upvoted + 1
-    elif c.score > 0:
-      print "\033[1;30m%s%s\033[1;m"%(spaces,c.score),
-      unvoted = unvoted + 1
-
-    sys.stdout.flush()
-
-  print ("")
-  log("COMMENT SCORE CHECK CYCLE COMPLETED")
-  urate = round(upvoted / float(total) * 100)
-  nrate = round(unvoted / float(total) * 100)
-  drate = round(downvoted / float(total) * 100)
-  warn("Upvoted:      %s\t%s\b\b %%"%(upvoted,urate))
-  warn("Unvoted       %s\t%s\b\b %%"%(unvoted,nrate))
-  warn("Downvoted:    %s\t%s\b\b %%"%(downvoted,drate))
-  warn("Total:        %s"%total)
-
-  try:
-    ss = open("subreddit_scores.%d.tsv" % (time.time()),"w")
-    for sr, score in subreddit_scores.iteritems():
-      ss.write("%s\t%d\n" % (sr, score)) 
-    ss.close()
-    dl = open("deleted_list.tsv","a")
-    for cols in del_list:
-      dl.write("\t".join(map(str, cols)) + "\n")
-    dl.close()
-  except Exception, e:
-    warn(e)
-    warn("Failed to write subreddit scores")
 
 def signal_handler(signum, frame):
   if(signum == signal.SIGHUP):
       load_settings()
 
+def cleanup():
+  global last_cleaned
 
-def load_settings():
-  global bans
-  global config
-  global imagemap
-  global r
+  if(last_cleaned and last_cleaned > (time.time() - bot.config['bot']['cleanup_time'])):
+    return
 
-  print "Reloading config..."
-  sys.stdout.flush()
-  config = yaml.load(open('config.yaml'))
-
-  #Load banlist
-  print "Loading banlist..."
-  sys.stdout.flush()
-  bottiquette = r.get_wiki_page('Bottiquette', 'robots_txt_json')
-  banlist = json.loads(bottiquette.content_md)
-  btqban = (banlist['disallowed'] +\
-      banlist['posts-only'] +\
-      banlist['permission'])
-
-  mybans = [x for x in list(open('blacklist.txt'))\
-      if not (x.strip() == '' or x.startswith('#'))]
-  
-  bans = [x.strip().lower() for x in (btqban + mybans)]
-  print "Ignoring subreddits: %s" % (', '.join(bans))
-
-  #Load image map
-  imageconf = yaml.load(open('imagelist.yaml'))
-  imagemap = load_imagelist(imageconf)
-
-  markdown = print_imagelist(imageconf)
-
-  shutil.copy('imagelist.md','imagelist.previous.md')
-  mdf = open('imagelist.md','w')
-  mdf.write(markdown)
-  mdf.close()
-
-  print "Loaded image map:"
-  pprint(imagemap)
-  sys.stdout.flush()
+  last_cleaned = time.time()
+  return bot.cleanup()
 
 parser = argparse.ArgumentParser(description="Links text such as themoreyouknow.gif to actual images")
 
-#Load config and set up
-print "Logging in..."
-config = yaml.load(open('config.yaml'))
-r = praw.Reddit('Image Text Linker by /u/cincodenada v0.1 at /r/image_linker_bot')
-r.login(config['account']['username'],config['account']['password'])
+global bot
+global last_cleaned
+global imagemap
 
-load_settings()
+bot = JoelBot('ilb_test')
+last_cleaned = 0
+
 signal.signal(signal.SIGHUP,signal_handler)
 
-ext_list = '|'.join(config['bot']['extensions'])
+#Load image map
+imageconf = yaml.load(open('imagelist.yaml'))
+imagemap = load_imagelist(imageconf)
+
+markdown = print_imagelist(imageconf)
+
+shutil.copy('imagelist.md','imagelist.previous.md')
+mdf = open('imagelist.md','w')
+mdf.write(markdown)
+mdf.close()
+
+print "Loaded image map:"
+pprint(imagemap)
+sys.stdout.flush()
+
+ext_list = '|'.join(bot.config['bot']['extensions'])
 maybeimage = re.compile(r'(^|\s|\^+)(\w+)\.(%s)\b' % (ext_list),re.IGNORECASE)
-
-#Load already-checked queue
-try:
-  already_seen = pickle.load(open('seen.pickle'))
-except Exception:
-  already_seen = collections.deque(maxlen=config['bot']['seen_len'])
-
-global last_cleaned
-last_cleaned = 0
 
 numchecked = 0
 while True:
   try:
-    for comment in praw.helpers.comment_stream(r, 'all', limit=None, verbosity=0):
+    for comment in bot.comment_stream:
       cleanup()
-
-      if comment.id in already_seen:
-        print "Already saw comment %s, skipping..." % (comment.id)
-        continue
-
-      already_seen.append(comment.id)
 
       if hasattr(comment,'body'):
         numchecked += 1
@@ -224,14 +111,7 @@ while True:
         foundkeys = []
         matches = maybeimage.findall(comment.body)
         if len(matches):
-          #Don't post in bot-banned subreddits
-          subreddit = comment.subreddit.display_name.lower()
-          if subreddit in bans:
-            print "Skipping banned subreddit %s" % (subreddit)
-            continue
-
-          #Don't reply to self, just in case...
-          if comment.author.name == config['account']['username']:
+          if(bot.should_ignore(comment)):
             continue
 
           for match in matches:
@@ -257,11 +137,11 @@ while True:
           
           if len(commentlinks):
             if(not comment.is_root):
-              parent = r.get_info(thing_id=comment.parent_id)
+              parent = bot.r.get_info(thing_id=comment.parent_id)
               subreddit = comment.subreddit.display_name.lower()
-              if(parent.author.name == config['account']['username'] and subreddit != config['account']['username']):
+              if(parent.author.name == bot.config['account']['username'] and subreddit != bot.config['account']['username']):
                 print "Sending warning to %s for reply-reply..." % (comment.author)
-                r.send_message(comment.author,'I\'m glad you like me, but...',config['bot']['toomuch'],raise_captcha_exception=True)
+                bot.r.send_message(comment.author,'I\'m glad you like me, but...',bot.config['bot']['toomuch'],raise_captcha_exception=True)
                 continue
 
             replytext = form_reply(commentlinks)
@@ -278,7 +158,7 @@ while True:
             
   except KeyboardInterrupt:
     print "Shutting down after scanning %d comments..." % (numchecked)
-    pickle.dump(already_seen,open('seen.pickle','w'))
+    bot.save_seen()
     sys.exit("Keyboard interrupt, shutting down...")
-  except Exception, e:
-    pprint(e)
+# except Exception, e:
+#   pprint(e)
---
+++ joelbot.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# vim: sw=2 ts=2 sts=2 et :
+import praw
+import time
+import yaml
+import json
+import pickle
+import collections
+from util import success, warn, log, fail
+import sys
+
+class JoelBot:
+  def __init__(self, subreddit, config_file='config.yaml'):
+    #Load config and set up
+    print "Logging in..."
+    self.config = yaml.load(open(config_file))
+    self.r = praw.Reddit(self.config['bot']['useragent'])
+    self.r.login(self.config['account']['username'],self.config['account']['password'])
+
+    self.comment_stream = UnseenComments(self.r, subreddit, self.config['bot']['seen_len'])
+    self.subreddit = subreddit
+
+    self.load_settings()
+
+  def load_settings(self):
+    print "Reloading config..."
+    sys.stdout.flush()
+    self.config = yaml.load(open('config.yaml'))
+
+    #Load banlist
+    print "Loading banlist..."
+    sys.stdout.flush()
+    bottiquette = self.r.get_wiki_page('Bottiquette', 'robots_txt_json')
+    banlist = json.loads(bottiquette.content_md)
+    btqban = (banlist['disallowed'] +\
+        banlist['posts-only'] +\
+        banlist['permission'])
+
+    mybans = [x for x in list(open('blacklist.txt'))\
+        if not (x.strip() == '' or x.startswith('#'))]
+    
+    self.bans = [x.strip().lower() for x in (btqban + mybans)]
+    print "Ignoring subreddits: %s" % (', '.join(self.bans))
+
+  def should_ignore(self, comment):
+    #Don't post in bot-banned subreddits
+    subreddit = comment.subreddit.display_name.lower()
+    if subreddit in self.bans:
+      print "Skipping banned subreddit %s" % (subreddit)
+      return True
+
+    #Don't reply to self, just in case...
+    if comment.author.name == self.config['account']['username']:
+      return True
+
+    return False
+
+  def save_seen(self):
+    self.comment_stream.save_state()
+
+  def cleanup(self):
+    subreddit_scores = {}
+
+    # Comment deletion taken straight from autowikibot
+    # No need to reinvent the wheel
+    log("COMMENT SCORE CHECK CYCLE STARTED")
+    user = self.r.get_redditor(self.config['account']['username'])
+    total = 0
+    upvoted = 0
+    unvoted = 0
+    downvoted = 0
+    deleted = 0
+    del_list = []
+            
+    for c in user.get_comments(limit=None):
+      
+      if len(str(c.score)) == 4:
+        spaces = ""
+      if len(str(c.score)) == 3:
+        spaces = " "
+      if len(str(c.score)) == 2:
+        spaces = "  "
+      if len(str(c.score)) == 1:
+        spaces = "   "
+      
+      #Keep track of our votes
+      sub = c.subreddit.display_name
+      if(sub not in subreddit_scores):
+        subreddit_scores[sub] = 0
+      subreddit_scores[sub] += c.score
+
+      total = total + 1
+
+      if c.score < 1: # or sub.lower() in self.bans:
+        del_list.append((sub.lower(), c.score, c.permalink))
+        c.delete()
+        print "\033[1;41m%s%s\033[1;m"%(spaces,c.score),
+        deleted = deleted + 1
+        downvoted = downvoted + 1
+      elif c.score > 10:
+        print "\033[1;32m%s%s\033[1;m"%(spaces,c.score),
+        upvoted = upvoted + 1
+      elif c.score > 1:
+        print "\033[1;34m%s%s\033[1;m"%(spaces,c.score),
+        upvoted = upvoted + 1
+      elif c.score > 0:
+        print "\033[1;30m%s%s\033[1;m"%(spaces,c.score),
+        unvoted = unvoted + 1
+
+      sys.stdout.flush()
+
+    print ("")
+    log("COMMENT SCORE CHECK CYCLE COMPLETED")
+    urate = round(upvoted / float(total) * 100)
+    nrate = round(unvoted / float(total) * 100)
+    drate = round(downvoted / float(total) * 100)
+    warn("Upvoted:      %s\t%s\b\b %%"%(upvoted,urate))
+    warn("Unvoted       %s\t%s\b\b %%"%(unvoted,nrate))
+    warn("Downvoted:    %s\t%s\b\b %%"%(downvoted,drate))
+    warn("Total:        %s"%total)
+
+    try:
+      ss = open("subreddit_scores.%d.tsv" % (time.time()),"w")
+      for sr, score in subreddit_scores.iteritems():
+        ss.write("%s\t%d\n" % (sr, score)) 
+      ss.close()
+      dl = open("deleted_list.tsv","a")
+      for cols in del_list:
+        dl.write("\t".join(map(str, cols)) + "\n")
+      dl.close()
+    except Exception, e:
+      warn(e)
+      warn("Failed to write subreddit scores")
+
+class UnseenComments:
+  def __init__(self, r, subreddit, maxlen=1000, state_file='seen.pickle'):
+    self.r = r
+    self.subreddit = subreddit
+    self.comment_stream = praw.helpers.comment_stream(self.r, self.subreddit, limit=None, verbosity=0)
+    self.state_file = state_file
+
+    #Load already-checked queue
+    try:
+      self.already_seen = pickle.load(open(state_file))
+    except Exception:
+      self.already_seen = collections.deque(maxlen=maxlen)
+
+  def __iter__(self):
+    return self
+
+  def next(self):
+    next_comment = self.comment_stream.next()
+    if next_comment.id in self.already_seen:
+      print "Already saw comment %s, skipping..." % (next_comment.id)
+      return self.next()
+
+    self.already_seen.append(next_comment.id)
+    return next_comment;
+
+  def save_state(self):
+    return pickle.dump(self.already_seen,open(self.state_file,'w'))
