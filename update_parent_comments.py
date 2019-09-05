6b56a93165c48f355df1a1e6255693f50550658d
Joel Bradshaw <cincodenada@gmail.com>
2019-09-04 18:54:47 -0700

    More tolerant and ignore some sorta messages

--- update_parent_comments.py
+++ update_parent_comments.py
@@ -9,10 +9,14 @@ conn.isolation_level = None
 c = conn.cursor()
 c_i = conn.cursor()
 
-for msg in c.execute('''SELECT tid FROM inbox WHERE parent_id IS NULL'''):
+for msg in c.execute('''SELECT tid FROM inbox WHERE parent_id IS NULL AND tid NOT LIKE \'t4_%\''''):
     msgid = msg[0]
     print "Checking {}...".format(msgid)
     data = bot.r.get_info(thing_id=msgid)
     if data and data.parent_id:
-        print "Updating message {} with parent id {}...".format(data.name, data.parent_id)
-        c_i.execute('''UPDATE inbox SET parent_id=? WHERE tid=?''', (data.name, data.parent_id))
+        try:
+            print "Updating message {} with parent id {}...".format(data.name, data.parent_id)
+            c_i.execute('''UPDATE inbox SET parent_id=? WHERE tid=?''', (data.parent_id, data.name))
+            conn.commit()
+        except sqlite3.OperationalError:
+            pass
