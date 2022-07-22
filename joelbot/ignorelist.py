3ce7f4df39025097f1d60b2956ee26637cb85b95
Joel Bradshaw <cincodenada@gmail.com>
2022-07-21 21:10:43 -0700

    Allow multiple ignorelists

--- joelbot/ignorelist.py
+++ joelbot/ignorelist.py
@@ -4,25 +4,27 @@ import sqlite3
 import time
 
 class IgnoreList():
-  def __init__(self, dbfilename):
+  def __init__(self, dbfilename, key="ignore"):
+    self.key = key
+
     self.conn = sqlite3.connect(dbfilename)
     self.conn.row_factory = sqlite3.Row
     #Autocommit
     self.conn.isolation_level = None
     self.c = self.conn.cursor()
 
-    self.c.execute('''CREATE TABLE IF NOT EXISTS ignore
-        (username TEXT, request_id TEXT, ignore_date INTEGER)''')
-    self.c.execute('''CREATE UNIQUE INDEX IF NOT EXISTS ignore_user ON ignore(username)''')
+    self.c.execute('''CREATE TABLE IF NOT EXISTS {0}
+        (username TEXT, request_id TEXT, {0}_date INTEGER)'''.format(self.key)
+    self.c.execute('''CREATE UNIQUE INDEX IF NOT EXISTS {0}_user ON {0}(username)'''.format(self.key))
 
   def ignore_sender(self, m):
-    self.c.execute('''INSERT OR REPLACE INTO ignore(username, request_id, ignore_date) VALUES(?,?,?)''',
+    self.c.execute('''INSERT OR REPLACE INTO {0}(username, request_id, {0}_date) VALUES(?,?,?)'''.format(self.key),
         (m.author.name, m.name, time.time()))
 
   def unignore_sender(self, m):
-    self.c.execute('''DELETE FROM ignore WHERE username=?''', (m.author.name,))
+    self.c.execute('''DELETE FROM {0} WHERE username=?'''.format(self.key), (m.author.name,))
 
   def check_ignored(self, username):
-    self.c.execute('''SELECT username FROM ignore WHERE username=? LIMIT 1''', (username,))
+    self.c.execute('''SELECT username FROM {0} WHERE username=? LIMIT 1'''.format(self.key), (username,))
     user = self.c.fetchone();
     return (user is not None)
