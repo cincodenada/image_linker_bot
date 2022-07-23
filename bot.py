d034e15d74dec485af4b5c9d10c3a781a6cb1f12
Joel Bradshaw <cincodenada@gmail.com>
2022-07-22 23:42:50 -0700

    Break up linkerbot loop, move some back into bot.py

--- bot.py
+++ bot.py
@@ -12,4 +12,72 @@ def handle_signal(signum, frame):
 signal.signal(signal.SIGHUP, handle_signal)
 
 bot = LinkerBot('all')
-bot.run()
+
+numchecked = 0
+numsamples = 0
+maxsamples = 1000
+update_period = 100
+totaltime = 0
+last_restart = time.time() - 10;
+sleep_secs = 5
+max_sleep = 2**16
+while True:
+  try:
+    #Sanity check: sleep a bit before logging things
+    #Also some simple escalation: double sleep time if
+    #We restart too quickly
+    time_since_last_restart = time.time() - last_restart
+    if(time_since_last_restart < sleep_secs*2):
+      if(sleep_secs < max_sleep):
+        sleep_secs = sleep_secs*2
+
+      #Be safe here cause we can spin into disk-eating death if we die before sleeping
+      try:
+        log("Restarted too quickly, refreshing comments and backing off to %d seconds...", sleep_secs)
+        if(comment and hasattr(comment, '__dict__')):
+          log("Last comment: %s", pformat(comment.__dict__))
+        else:
+          log("Last comment: %s", pformat(comment))
+      except Exception, e:
+        print traceback.format_exc()
+    else:
+      sleep_secs = 5
+    time.sleep(sleep_secs)
+
+    bot.start()
+
+    log("Starting comment stream...")
+    last_restart = time.time()
+    while True:
+      start = time.time();
+      try:
+        bot.next()
+        numchecked += 1
+      except EmptyBodyError, e:
+        log("Comment without body: %s", e.message)
+        pass
+      duration = time.time() - start
+
+      totaltime += duration
+      numsamples += 1
+      if(numchecked % update_period == 0):
+        log("\rChecked %d comments...",(numchecked),stderr=True,newline=False)
+      if(numsamples >= maxsamples):
+        log("Average processing time of last %d comments: %.2f ms",(numsamples, totaltime/numsamples*1000))
+        numsamples = 0
+        totaltime = 0
+
+      sys.stdout.flush()
+
+    # Maybe if we get to the end, we need to get more?
+    bot.refresh_comments()
+
+  except prawcore.exceptions.OAuthException:
+    bot.refresh_oauth()
+  except KeyboardInterrupt:
+    log("Shutting down after scanning %d comments...",(numchecked))
+    bot.save_seen()
+    sys.exit("Keyboard interrupt, shutting down...")
+  except Exception, e:
+    log(u"Error!")
+    print traceback.format_exc()
--- linkerbot.py
+++ linkerbot.py
@@ -18,6 +18,9 @@ from joelbot.util import log, get_sender
 from imagemap import ImageMap
 from memedb import MemeDb
 
+class EmptyBodyError(RuntimeError):
+  pass
+
 class LinkerBot(JoelBot):
   def __init__(self, *args, **kwargs):
     JoelBot.__init__(self, *args, **kwargs)
@@ -36,6 +39,9 @@ class LinkerBot(JoelBot):
     self.maybeimage = re.compile(r'(^|\s|\^+)(\w+)\.(%s)\b' % (ext_list),re.IGNORECASE)
 
     self.generate_statuspage()
+
+    log("Opening database...")
+    self.memes = MemeDb(self.config['dbfile'])
 
   def form_reply(self, link_list, footer = 'footer'):
     lines = []
@@ -116,159 +122,122 @@ class LinkerBot(JoelBot):
         header = header.group(1)
         imagepost.edit("%s---\n%s" % (header, markdown))
 
-  def run(self):
-    numchecked = 0
-    numsamples = 0
-    maxsamples = 1000
-    update_period = 100
-    totaltime = 0
-    last_restart = time.time() - 10;
-    sleep_secs = 5
-    max_sleep = 2**16
-    while True:
-      try:
-        #Sanity check: sleep a bit before logging things
-        #Also some simple escalation: double sleep time if
-        #We restart too quickly
-        time_since_last_restart = time.time() - last_restart
-        if(time_since_last_restart < sleep_secs*2):
-          if(sleep_secs < max_sleep):
-            sleep_secs = sleep_secs*2
+  def start(self):
 
-          #Be safe here cause we can spin into disk-eating death if we die before sleeping
-          try:
-            log("Restarted too quickly, refreshing comments and backing off to %d seconds...", sleep_secs)
-            if(comment and hasattr(comment, '__dict__')):
-              log("Last comment: %s", pformat(comment.__dict__))
-            else:
-              log("Last comment: %s", pformat(comment))
-          except Exception, e:
-            print traceback.format_exc()
-        else:
-          sleep_secs = 5
-        time.sleep(sleep_secs)
+  def next(self):
+    comment = next(self.comment_stream)
+    if not hasattr(comment, 'body'):
+      raise EmptyBodyError(str(comment))
 
-        log("Opening database...")
-        memes = MemeDb(self.config['dbfile'])
+    matches = self.maybeimage.findall(comment.body)
+    if len(matches):
+      # Do this here because it's a db call, don't want to do it unless we match
+      if(self.should_ignore(comment)):
+        return
 
-        log("Starting comment stream...")
-        last_restart = time.time()
-        for comment in self.comment_stream:
-          start = time.time();
+      commentlinks = self.get_links(matches)
+      if not commentlinks:
+        return
 
-          if hasattr(comment,'body'):
-            numchecked += 1
+      if self.is_reply_reply(comment):
+        reply = self.reply_warn(comment, commentlinks)
+      else:
+        reply = self.reply(comment, commentlinks)
 
-            commentlinks = collections.OrderedDict()
-            foundkeys = []
-            matches = self.maybeimage.findall(comment.body)
-            if len(matches):
-              if(self.should_ignore(comment)):
-                continue
+      self.memes.addComment(comment, sent)
 
-              ts = time.time()
-              for match in matches:
-                # Add the match to the list if it's not a dup
-                (prefix, key, ext) = match
-                # Strip out underscores/hyphens
-                searchkey = key.lower().replace('_','').replace('-','')
-                # Remove "meme" from the end
-                if searchkey.endswith('meme'):
-                  searchkey = searchkey[:-4]
-                (urls, imagekey) = self.imagemap.get(searchkey, ext)
-                if urls:
-                  if imagekey not in foundkeys:
-                    foundkeys.append(imagekey)
+  def get_links(self, matches):
+    ts = time.time()
+    commentlinks = collections.OrderedDict()
+    foundkeys = []
 
-                    linktext = "%s.%s" % (key,ext)
-                    if(len(prefix.strip()) > 0):
-                      linktext = prefix + linktext
+    for match in matches:
+      # Add the match to the list if it's not a dup
+      (prefix, key, ext) = match
+      # Strip out underscores/hyphens
+      searchkey = key.lower().replace('_','').replace('-','')
+      # Remove "meme" from the end
+      if searchkey.endswith('meme'):
+        searchkey = searchkey[:-4]
+      (urls, imagekey) = self.imagemap.get(searchkey, ext)
+      if urls:
+        if imagekey not in foundkeys:
+          foundkeys.append(imagekey)
 
-                    url = random.choice(urls)
-                    commentlinks[linktext] = url
-                    memes.addMatch(comment, key, ext, ts, imagekey, url)
-                else:
-                  memes.addCandidate(comment, key, ext, ts)
-                  log(u"\nPossible new image for %s\n%s",(comment.permalink, u' '.join(match)))
-              
-              if len(commentlinks):
-                if(not comment.is_root):
-                  parent = self.r.comment(comment.parent_id[3:])
-                  subreddit = comment.subreddit.display_name.lower()
-                  if(parent.author and parent.author.name == self.username and subreddit != self.username):
-                    log("Sending warning to %s for reply-reply...",(comment.author))
+          linktext = "%s.%s" % (key,ext)
+          if(len(prefix.strip()) > 0):
+            linktext = prefix + linktext
 
-                    #Always with the plurals
-                    plural = 'are the images'
-                    if(len(commentlinks) > 1):
-                      plural = 'are the images'
-                    else:
-                      plural = 'is the image'
+          url = random.choice(urls)
+          commentlinks[linktext] = url
+          self.memes.addMatch(comment, key, ext, ts, imagekey, url)
+      else:
+        self.memes.addCandidate(comment, key, ext, ts)
+        log(u"\nPossible new image for %s\n%s",(comment.permalink, u' '.join(match)))
 
-                    #Construct our message
-                    message = 'Here %s you wanted: %s\n\n%s' % (
-                      plural,
-                      self.form_reply(commentlinks, None),
-                      self.config['toomuch']
-                    )
+    if len(commentlinks):
+      return commentlinks
+    else:
+      return None
 
-                    self.r.redditor(comment.author).message('I\'m glad you like me, but...',message,raise_captcha_exception=True)
-                    memes.addComment(comment, message)
-                    continue
+  def is_reply_reply(self, comment):
+    if comment.is_root:
+      return False
 
-                replytext = self.form_reply(commentlinks)
-                try:
-                  log("Commenting on %s (%s)",(comment.permalink, ', '.join(commentlinks.keys())))
-                  comment.reply(replytext)
-                except praw.exceptions.APIException, e:
-                  if(e.error_type == "TOO_OLD"):
-                    log("Comment too old!")
-                  elif(e.error_type == "SUBREDDIT_OUTBOUND_LINKING_DISALLOWED"):
-                    log("Commenting on %s without link:",(comment.permalink))
-                    replytext = self.form_reply(commentlinks, 'nolink_footer')
-                    comment.reply(replytext)
-                  else:
-                    # Otherwise assume it's rate limiting...without a sleep time?...ugh
-                    sleeptime = 2
-                    log("Rate limit exceeded, sleeping %d seconds and trying again...",(sleeptime))
-                    time.sleep(sleeptime)
-                    log("Re-commenting on %s",(comment.permalink))
-                    comment.reply(replytext)
-                except prawcore.exceptions.ResponseException, e:
-                  log("Got response error: %s", (e))
-                  log(e.response.text)
-                  if e.response.status_code == 403:
-                    # TODO: get_sender being external here is ick
-                    self.do_command('ban', get_sender(comment), reason="comment_forbidden")
-                  else:
-                    raise e
+    parent = self.r.comment(comment.parent_id[3:])
+    subreddit = comment.subreddit.display_name.lower()
+    return parent.author and parent.author.name == self.username and subreddit != self.username
 
-                memes.addComment(comment, replytext)
+  def reply(self, comment, commentlinks):
+    replytext = self.form_reply(commentlinks)
+    try:
+      log("Commenting on %s (%s)",(comment.permalink, ', '.join(commentlinks.keys())))
+      comment.reply(replytext)
+    except praw.exceptions.APIException, e:
+      if(e.error_type == "TOO_OLD"):
+        log("Comment too old!")
+      elif(e.error_type == "SUBREDDIT_OUTBOUND_LINKING_DISALLOWED"):
+        log("Commenting on %s without link:",(comment.permalink))
+        replytext = self.form_reply(commentlinks, 'nolink_footer')
+        comment.reply(replytext)
+      else:
+        # Otherwise assume it's rate limiting...without a sleep time?...ugh
+        sleeptime = 2
+        log("Rate limit exceeded, sleeping %d seconds and trying again...",(sleeptime))
+        time.sleep(sleeptime)
+        log("Re-commenting on %s",(comment.permalink))
+        comment.reply(replytext)
+    except prawcore.exceptions.ResponseException, e:
+      log("Got response error: %s", (e))
+      log(e.response.text)
+      if e.response.status_code == 403:
+        # TODO: get_sender being external here is ick
+        self.do_command('ban', get_sender(comment), reason="comment_forbidden")
+      else:
+        raise e
 
-          duration = time.time() - start
-          totaltime += duration
-          numsamples += 1
-          if(numchecked % update_period == 0):
-            log("\rChecked %d comments...",(numchecked),stderr=True,newline=False)
-          if(numsamples >= maxsamples):
-            log("Average processing time of last %d comments: %.2f ms",(numsamples, totaltime/numsamples*1000))
-            numsamples = 0
-            totaltime = 0
-          
-          sys.stdout.flush()
+    return replytext
 
-        # Maybe if we get to the end, we need to get more?
-        self.refresh_comments()
-                
-      except prawcore.exceptions.OAuthException:
-        self.refresh_oauth()
-      except KeyboardInterrupt:
-        log("Shutting down after scanning %d comments...",(numchecked))
-        self.save_seen()
-        sys.exit("Keyboard interrupt, shutting down...")
-      except Exception, e:
-        log(u"Error!")
-        print traceback.format_exc()
+  def reply_warn(self, comment, commentlinks):
+    log("Sending warning to %s for reply-reply...",(comment.author))
+
+    #Always with the plurals
+    plural = 'are the images'
+    if(len(commentlinks) > 1):
+      plural = 'are the images'
+    else:
+      plural = 'is the image'
+
+    #Construct our message
+    message = 'Here %s you wanted: %s\n\n%s' % (
+      plural,
+      self.form_reply(commentlinks, None),
+      self.config['toomuch']
+    )
+
+    self.r.redditor(comment.author).message('I\'m glad you like me, but...',message,raise_captcha_exception=True)
+    return message
+
 
 bot = LinkerBot('all')
 bot.run()
